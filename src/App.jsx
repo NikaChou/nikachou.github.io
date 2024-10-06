1c4766f29acdbd103c382aa175833823ff8e1c88
NikaChou <monikazhou@foxmail.com>
2024-10-06 16:22:24 +0800

    v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-import self_photo from './assets/seaside.png'; // 使用require或import导入图片
+import self_photo from './assets/seaside.JPG'; // 使用require或import导入图片
 import img_linkedin from './assets/linkedin.png';
 import img_github from './assets/github.svg';
 import img_twitter from './assets/twitter.svg';
 import img_redbook from './assets/Xiaohongshu.png';
+import logoBaidu from './assets/baidu_logo.jpg'; // 替换为实际路径
+import logoIFLYTEK from './assets/iflytek_logo.jpg'; // 替换为实际路径
+import logoGM from './assets/gm_logo.png'; // 替换为实际路径
+import logoHashKey from './assets/hashkey_logo.png'; // 替换为实际路径
 
 
 function App() {
@@ -52,34 +56,69 @@ function App() {
   ];
 
   const campus_activities = [
-    '2023-Now President of Fudan Job Hunt Club',
-    '2024-Now Vice President of Fudan University Blockchain Association',
+    '2023-Now President, Fudan Job Hunt Club',
+    '2024-Now Vice President, Fudan University Blockchain Association',
     '2022-2023 Core Member of Fudan University Graduate Admissions Promotion Team',
     '2020-2021 Director of External Relations, Students\' Union, Huazhong University of Science and Technology',
     '2019-2021 Core Member of University History Museum, Huazhong University of Science and Technology'
   ];
+
   const innovation_practice_projects = [
-    '2020-2021 Provincial Project, Project Leader, "19th Century German Writings on the History of Chinese Literature."',
-    '2019-2020 University Project, Participant, "The History and Policies of Waste Classification in Germany and Their Significance for Waste Classification Policies in China.'
+    {
+      title: "Provincial Project: 19th Century German Writings on the History of Chinese Literature.",
+      description: "2020-2021 , Project Leader",
+    },
+    {
+      title: 'University Project: The History and Policies of Waste Classification in Germany and Their Significance for Waste Classification Policies in China.',
+      description: '2019-2020 , Core Member',
+    },
   ];
 
 
   const internships = [
     {
-      title: 'Strategic Analysis in Autonomous Driving Group @Baidu (China) Co., Ltd.',
-      description: "07/2024 - 09/2024. \nConducted a study on the IPO of WeRide, produced 2 reports and 10 core insights for references. \nConducted market analysis based on foreign investment research reports, producing 7 weekly reports.",
+      title: 'Strategic Analysis @Baidu (China) Co., Ltd.',
+      logo: logoBaidu,
+      description: (
+        <>
+          2024, Intern, in Autonomous Driving Group <br />
+          Conducted a study on the IPO of WeRide, produced 2 reports and 10 core insights for references. <br />
+          Conducted market analysis based on foreign investment research reports, producing 7 weekly reports.
+        </>
+      ),
     },
     {
-      title: 'Voice Interaction Product Manager @iFLYTEK Co., Ltd.',
-      description: "04/2024 - 07/2024. \nAssisted in multilingual TTS business, resulting in 3 detailed feature lists. \nAnalyzed key indicators for improved performance: 5pt in recognition rate and 3pt in wake-up rate.",
+      title: 'Product Manager @iFLYTEK Co., Ltd.',
+      logo: logoIFLYTEK,
+      description: (
+        <>
+          2024, Intern, Voice Interaction APP <br />
+          Assisted in multilingual TTS business, resulting in 3 detailed feature lists. <br />
+          Analyzed key indicators for improved performance: 5pt in recognition rate and 3pt in wake-up rate.
+        </>
+      ),
     },
     {
-      title: 'Market Research and Portfolio Planning @General Motors Company',
-      description: "01/2024 - 04/2024. \nConducted research on vehicle and consumer, producing 2 insightful reports with 2 recommendations adopted. \nPrepared reports for an imported SUV case with quantitative and qualitative research.",
+      title: 'Market Research @General Motors Company',
+      logo: logoGM,
+      description: (
+        <>
+          2024, Intern, in Planning Department <br />
+          Conducted research on vehicle and consumer, producing 2 insightful reports with 2 recommendations adopted. <br />
+          Prepared reports for an imported SUV case with quantitative and qualitative research.
+        </>
+      ),
     },
     {
-      title: 'Market Research (ToB Cloud Products) @HashKey Group',
-      description: "06/2023 - 10/2023. \nCompleted market analysis and forecasts for target regions, proposing 4 recommendations adopted by the team. \nAnalyzed competitive products with in-depth research on 2 strong competitors.",
+      title: 'Market Research @HashKey Group',
+      logo: logoHashKey,
+      description: (
+        <>
+          2023, Intern, in ToB Cloud Products Department <br />
+          Completed market analysis and forecasts for target regions, proposing 4 recommendations adopted by the team. <br />
+          Analyzed competitive products with in-depth research on 2 strong competitors.
+        </>
+      ),
     },
   ];
 
@@ -92,10 +131,13 @@ function App() {
   ];
 
   const Interests = [
-    'AI & LLM',
-    'Blockchain & Web3',
+    'LLM',
+    'AI-Agent',
+    'Blockchain',
+    'Web3',
     'Autonomous Driving',
-    'Quantitative Trading',
+    'Office Automation',
+    'Global Social Media Marketing',
   ];
 
   return (
@@ -103,7 +145,8 @@ function App() {
       <div class="flex flex-col md:flex-row gap-4">
         <div class="container basis-1/4 bg-gradient-to-b from-gray-200 rounded-xl flex-col space-y-8 p-4 max-w-sm mx-auto">
           <img  src={self_photo} className="mx-auto rounded-3xl shadow-xl" />
-          <h1 className="text-2xl font-bold text-primary">Zhengzhi Zhou(NikaChou)</h1>
+          <h1 className="text-2xl font-bold text-primary">Zhengzhi Zhou</h1>
+          <h2 className="text-lg font-bold text-primary">(Nika Chou)</h2>
 
           <div>
             <h2 className="text-lg font-bold">Basic Info</h2>
@@ -113,9 +156,9 @@ function App() {
               <div>{personalInfo.contact}</div>
               <div className='flex flex-row justify-center mt-4'>
                 <a href={personalInfo.linkedin} target="_blank"><img className="w-6 h-6 mx-1 hover:shadow-lg" src={img_linkedin} alt='linkedin'/></a>
-                <a href={personalInfo.github} target="_blank"><img className="w-6 h-6 mx-1 hover:shadow-lg" src={img_github} alt='github'/></a>
                 <a href={personalInfo.twitter} target="_blank"><img className="w-6 h-6 mx-1 hover:shadow-lg" src={img_twitter} alt='twitter'/></a>
                 <a href={personalInfo.redbook} target="_blank"><img className="w-6 h-6 mx-1 hover:shadow-lg" src={img_redbook} alt='redbook'/></a>
+                <a href={personalInfo.github} target="_blank"><img className="w-6 h-6 mx-1 hover:shadow-lg" src={img_github} alt='github'/></a>
               </div>
             </div>
           </div>
@@ -149,6 +192,7 @@ function App() {
 
           <section>
             <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-4">Awards and Honors</h2>
+            <div className='text-lg pt-2 pb-1 font-bold'>Campus Honors</div>
             {honors.map((honor, index) => (
               <li key={index} className='rounded-lg hover:bg-gradient-to-r from-primary'>{honor}</li>
             ))}
@@ -163,7 +207,9 @@ function App() {
             <div className="grid grid-cols-1 gap-6">
               {academic_Achievements.map((achievement, index) => (
                 <div key={index} className="card rounded-lg shadow-md p-4 bg-gray-100 hover:bg-gray-300">
-                  <h3 className="text-lg font-semibold mb-2">{achievement.title}</h3>
+                  <h3 className="text-lg font-semibold mb-2">
+                    {achievement.title.replace('German Studies', <strong>German Studies</strong>).replace('Shanghai Graduate Education', <strong>Shanghai Graduate Education</strong>)}
+                  </h3>
                   <div className="text-gray-600">{achievement.description}</div>
                 </div>
               ))}
@@ -180,20 +226,25 @@ function App() {
 
           <section className="mb-8">
             <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-4">Innovation Practice Projects</h2>
-            <ul className="list-disc space-y-2">
+            <div className="grid grid-cols-1 gap-2">
               {innovation_practice_projects.map((project, index) => (
-                <div key={index} className="card rounded-lg shadow-md bg-gray-100 p-2 hover:bg-gray-300">{project}</div>
+                <div key={index} className="card rounded-lg shadow-md bg-gray-100 p-2 hover:bg-gray-300">
+                  <h3 className="text-lg font-semibold mb-2">{project.title}</h3>
+                  <div className="text-gray-600">{project.description}</div>
+                </div>
               ))}
-            </ul>
-          </section>
+            </div>
 
           <section className="mb-8">
             <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold mb-4">Internship Experience</h2>
             <ul className="list-disc space-y-2">
               {internships.map((internship, index) => (
-                <div key={index} className="card rounded-lg shadow-md p-4 bg-gray-100 hover:bg-gray-300">
-                  <h3 className="text-lg font-semibold mb-2">{internship.title}</h3>
-                  <div className="text-gray-600">{internship.description}</div> 
+                <div key={index} className="card rounded-lg shadow-md p-4 bg-gray-100 hover:bg-gray-300 flex items-center">
+                  <img src={internship.logo} alt={`${internship.title} logo`} className="w-10 h-10 mr-2" />
+                  <div>
+                    <h3 className="text-lg font-semibold mb-2">{internship.title}</h3>
+                    <div className="text-gray-600">{internship.description}</div>
+                  </div>
                 </div>
               ))}
             </ul>
